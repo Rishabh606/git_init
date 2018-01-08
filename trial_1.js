1788455cdee4714cac2e7aec2e3d98578410f9b1
Rishabh606 <gupta.rishabh606@gmail.com>
2018-01-08 23:59:36 +0530

    keep trying, will learn someday

--- trial_1.js
+++ trial_1.js
@@ -1,4 +1,15 @@
 /**
  * Created by rishabh on 08-01-2018.
  */
-console.log("Hey man").
+//console.log("Hey man").
+window.onload = function () {
+    console.log("hey man");
+    justanything();
+};
+function justanything() {
+    var todo = new array;
+    todo.push({
+        task:"done",
+        key:"bored"
+    });
+}
